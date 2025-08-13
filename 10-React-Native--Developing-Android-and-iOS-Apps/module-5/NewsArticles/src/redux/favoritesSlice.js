037c59bb3910358b824b10435d1efec0f3b67b55
Mehmet Tarhan <hi@memtarhan.com>
2025-08-13 13:15:24 +0100

    <ReactNative>: News Articles project

---
+++ 10-React-Native--Developing-Android-and-iOS-Apps/module-5/NewsArticles/src/redux/favoritesSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    favoriteArticles: [], // Updated to handle favorite articles
+};
+
+const favoritesSlice = createSlice({
+    name: "favorites",
+    initialState,
+    reducers: {
+
+    },
+});
+
+export const { toggleFavorite } = favoritesSlice.actions;
+export default favoritesSlice.reducer;
---
+++ 10-React-Native--Developing-Android-and-iOS-Apps/module-5/NewsArticles/src/screens/CustomNewsScreen.js
@@ -0,0 +1,128 @@
+import {
+    View,
+    Text,
+    ScrollView,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleFavorite } from "../redux/favoritesSlice";
+
+export default function CustomNewsScreen() {
+    const navigation = useNavigation();
+    const dispatch = useDispatch();
+
+    const route = useRoute();
+    const { article } = route.params || {}; // Pass the article object as a parameter
+    const favoriteArticles = useSelector(
+        (state) => state.favorites.favoriteArticles
+    );
+    const isFavourite = favoriteArticles.includes(article.idArticle); // Adjust this according to your article structure
+
+    if (!article) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>No Article Details Available</Text>
+            </View>
+        );
+    }
+
+    const handleToggleFavorite = () => {
+        dispatch(toggleFavorite(article)); // Adjust the action to handle articles
+    };
+
+    return (
+        <ScrollView
+            style={styles.container}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={styles.scrollContent} testID="scrollContent"
+        >
+            {/* Article Image */}
+            <View style={styles.imageContainer} testID="imageContainer">
+
+            </View>
+            <View
+                style={styles.topButtonsContainer} testID="topButtonsContainer"
+            >
+
+            </View>
+
+            {/* Article Details */}
+            <View style={styles.contentContainer} testID="contentContainer">
+
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        flex: 1,
+    },
+    scrollContent: {
+        paddingBottom: 30,
+    },
+    imageContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+    },
+    articleImage: {
+        width: wp(98),
+        height: hp(50),
+        borderRadius: 35,
+        borderBottomLeftRadius: 40,
+        borderBottomRightRadius: 40,
+        marginTop: 4,
+    },
+    contentContainer: {
+        paddingHorizontal: wp(4),
+        paddingTop: hp(4),
+    },
+    articleTitle: {
+        fontSize: hp(3),
+        fontWeight: "bold",
+        color: "#4B5563",
+        marginBottom: hp(2),
+    },
+    sectionContainer: {
+        marginBottom: hp(2),
+    },
+    sectionTitle: {
+        fontSize: hp(2.5),
+        fontWeight: "bold",
+        color: "#4B5563",
+        marginBottom: hp(1),
+    },
+    topButtonsContainer: {
+        width: "100%",
+        position: "absolute",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingTop: hp(4),
+    },
+    backButton: {
+        padding: 8,
+        borderRadius: 50,
+        marginLeft: wp(5),
+        backgroundColor: "white",
+    },
+    favoriteButton: {
+        padding: 8,
+        borderRadius: 50,
+        marginRight: wp(5),
+        backgroundColor: "white",
+    },
+    contentText: {
+        fontSize: hp(1.6),
+        color: "#4B5563",
+    },
+});
